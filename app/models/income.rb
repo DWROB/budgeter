536adb5ff3c7a70448f4f97412d9022cf8acf0a3
Darren Robinson <dwr@Darrens-MBP.lan>
2024-03-24 15:51:20 +0000

    20240324 - seeds

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,3 +1,3 @@
 class Income < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, optional: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,73 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts 'seeding beginning...'
+user = User.find(1)
+
+puts "#{user.username} is the user"
+
+puts 'making expense types ...'
+
+ExpenseType.destroy_all
+
+expense_types = [
+    'home',
+    'groceries',
+    'eating out',
+    'entertainment',
+    'vet'
+]
+
+expense_types.each do |type|
+    #create some expense types
+    ExpenseType.create(
+        name: type,
+        flg_deleted: false
+    )
+
+end
+
+puts 'making expenses ...'
+
+Expense.destroy_all
+
+50.times do
+    # create expenses
+    # randomly assign a value from 0 - 7
+    Expense.create(
+        supplier: Faker::Company.name,
+        expense_types_id: ExpenseType.all.sample.id,
+        amount: Faker::Number.decimal(l_digits: 2)
+    )
+end
+
+puts 'making income types...'
+
+IncomeType.destroy_all
+
+income_types = [
+    'salary',
+    'gift',
+    'interest'
+]
+
+income_types.each do |type|
+   IncomeType.create(name: type)
+end
+
+puts 'making incomes ...'
+
+Income.destroy_all
+
+10.times do
+    # create some incomes
+    Income.create(
+        source: Faker::Bank.name,
+        income_types_id: IncomeType.all.sample.id,
+        amount: Faker::Number.decimal(l_digits: 2)
+    )
+end
+
+puts 'done'
