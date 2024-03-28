ae5e2dd0f7faac362cbe5ca209f6c01ef680db3d
Darren Robinson <dwr@Darrens-MBP.lan>
2024-03-28 20:17:40 +0000

    20240328 - basic paths for user and expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,3 +1,26 @@
 class ExpensesController < ApplicationController
 
+    def index
+       Expense.where(users_id: params[:user_id])
+    end
+
+    def show
+        Expense.where(id: params[:id])
+    end
+
+    def create
+        Expense.create(expense_params)
+    end
+
+    def edit
+       @expense = Expense.find(id: params[:id])
+
+       @expense.update_attributes(expense_params)
+    end
+
+    private
+
+    def expense_params
+       params.require(:expense).permit(:supplier, :users_id, :expense_types_id, :amount)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,32 @@ class User < ApplicationRecord
     has_many :incomes
     has_many :target_expenses
     has_one :balance
+
+    def create
+        @new_user = User.new(user_params)
+
+        if @new_user.save!
+           return { success: true, user: @new_user }
+        else
+            return 'save failed'
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+
+        if @user.update_attributes(params)
+            return { success: true, user: @user }
+        else
+            return 'user update failed'
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :location)
+    end
+
+
 end
